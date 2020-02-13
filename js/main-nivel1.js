a018b14abd3940ee5c7e9195bbb72be2d18d115f
Luigi <luigi_lipreri@hotmail.com>
2020-02-12 21:14:50 -0300

    Agregada la funcion validarNombre

--- js/main-nivel1.js
+++ js/main-nivel1.js
@@ -1,1 +1,26 @@
-console.log("js cargado!");
+
+
+function validarNombre(nombre) {
+
+    if (nombre.length === 0) {
+  
+      return "El campo nombre debe tener al menos 1 caracter";
+  
+    }
+  
+    if (nombre.length >= 50) {
+  
+      return "El campo nombre debe tener menos de 50 caracteres";
+  
+    }
+  
+    if (!/^[a-z]+$/i.test(nombre)) {
+  
+      return "El campo nombre sólo acepta letras";
+  
+    }
+  
+    return "";
+  
+  }
+  
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,1 +1,25 @@
+function probarValidarNombre() {
+    console.assert(
+        validarNombre('') === "El campo nombre debe tener al menos 1 caracter",
+        'Validar nombre no validó que el nombre no sea vacío'
+    );
+  
+    console.assert(
+      validarNombre(
+            "123456789123456789123456789123456789123456789123456789123456789123456789") ==="El campo nombre debe tener menos de 50 caracteres",
+            "Validar nombre no validó que el campo nombre tenga menos de 50 caracteres"
+    );
+  
+    console.assert(
+      validarNombre("123456789")==="El campo nombres solo acepta letras",
+      "la funcion validarNombre no valido que el campo solo contega letras."
+    );
+  
+    console.assert(
+        validarNombre("Luigi")==="",
+        "la funcion validarNombre fallo con un nombre valido."
+    );
+  
+  }
 
+  probarValidarNombre();
