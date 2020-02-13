42ca5717e687787a9d11670177030e356b233476
Luigi <luigi_lipreri@hotmail.com>
2020-02-12 23:23:05 -0300

    Agregada la funcion para validar el formulario y manejar un objde errores

--- js/main-nivel1.js
+++ js/main-nivel1.js
@@ -54,3 +54,46 @@ function validarDescripcionRegalo(descripcionRegalo) {
         return "";
     }
 }
+
+function validarFormulario(event){
+  const $form = document.querySelector("#carta-a-santa");
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form['descripcion-regalo'].value;
+
+  const errorNombre = validarNombre(nombre);
+  const errorCiudad = validarCiudad(ciudad);
+  const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+  const objErrores = {
+      nombre:errorNombre,
+      ciudad:errorCiudad,
+      'descripcion-regalo':errorDescripcionRegalo
+
+  };
+  
+  manejarErrores(objErrores);
+  event.preventDefault();
+
+}
+
+function manejarErrores(errores){
+
+ const keys = Object.keys(errores);
+ 
+ keys.forEach(function(key) {
+    const error = errores[key];
+  
+  if(error){
+      $form[key].className = "error";
+  }else{
+      $form[key].className= "";
+  }
+
+ });
+
+
+}
+
+const $form = document.querySelector("#carta-a-santa");
+$form.onsubmit = validarFormulario;
